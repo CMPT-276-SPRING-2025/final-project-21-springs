2a9445b4b22e1b1dcfd9a26e04445c293e66b8a4
Rheana Lopez <rheana.l@yahoo.com>
2025-04-05 12:54:26 -0700

    delete unused tests

--- src/JS/load-list.js
+++ src/JS/load-list.js
@@ -15,7 +15,6 @@ function displayUsers(users, listId) {
     const usersList = users.map(user => `${user.firstName}`).join(', ');
     userArray = users.map(user => `${user.firstName}`);
     document.getElementById(listId).textContent = usersList;
-    console.log("Member list:", userArray);
 }
 
 function fetchThenDisplayBasic(start, end, userIds) {
@@ -110,4 +109,3 @@ async function updateTaskCompletion(taskId, isCompleted) {
 }
 
 document.addEventListener("DOMContentLoaded", loadLeaderboard);
-export { fetchUsers };
